f409270eca49fab14dd90998f7c0b42d0d7d06ef
Mohammadmahdi <mahdialvansaz@gmail.com>
2022-11-03 21:11:03 +0330

    delete unused files and do some minor changes.

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,22 @@
   <main class="px-4 md:px-16 mt-6 md:mt-16">
     <div class="text-lg md:text-xl mb-8">Hi 👋🏻😄</div>
     <h1 class="font-black text-4xl md:text-6xl mb-3">I'm Mohammadmahdi,</h1>
-    <h2 class="font-extralight text-2xl md:text-4xl mb-14">a frontend developer.</h2>
+    <h2 class="font-extralight text-2xl md:text-4xl mb-14">
+      a frontend developer.
+    </h2>
     <hr class="-rotate-2 border-t-4 mb-16" />
-    <p class="text-lg md:text-2xl mb-10 md:mb-14">You can contact me in the following ways. 👇🏻</p>
+    <p class="text-lg md:text-2xl mb-10 md:mb-14">
+      You can contact me in the following ways. 👇🏻
+    </p>
     <div class="flex flex-col md:flex-row items-center gap-4 md:gap-8">
-      <div
+      <a
         class="py-2 w-40 flex justify-center items-center rounded-sm font-semibold border-4 border-darker dark:border-lighter hover:bg-light dark:hover:bg-dark transition-colors cursor-pointer"
         v-for="contactBtn in contactBtns"
         :key="contactBtn.text"
+        :href="contactBtn.link"
       >
-        <a :href="contactBtn.link">
-          {{ contactBtn.text }}
-        </a>
-      </div>
+        {{ contactBtn.text }}
+      </a>
     </div>
   </main>
 </template>
